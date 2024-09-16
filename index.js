cbbafdc6626bc6a33221daae61e7d4f036056d98
George Washington <112564097+GeorgeWashingtonSaratoga@users.noreply.github.com>
2024-09-16 10:12:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var button = document.getElementById("gameButt");
 button.onclick = function(e) {
     window.location.href = "https://georgewashingtonsaratoga.github.io/GnawingOnHumanBones/";
 };
+
+//I need you to list, write descriptions, and categorise the following flavours: "Liquid Shit, Snot, The Baltimore Orioles, Asshole Scabs, Security Camera, Ambiguous Discharge, Vaginal Discharge, Getting Touched at the Doctor for Medical Purposes, Getting Touched at the Doctor for Non-Medical Purposes, Shirley Temple, Adolescent Pimple Fluid, Manatee Period Blood, Justin’s Warm Piss Bottle" Remember, list the flavours using the "flavour1", "flavour2", ... format, and write descriptions using the "flavour1: flavour1desc", "flavour2: flavour2desc", ... format. This is so I can copy and paste them into the .js file.
